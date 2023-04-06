69ffeed683940491c7e8462ed803e2c27a163c47
t776186 <daniel.baiao@santander.com.br>
2023-04-06 09:57:44 -0300

    exclusion of users

--- users/src/main/java/br/com/ada/users/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ada.users.repository;
-
-import br.com.ada.users.model.entity.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, String> {
-}
